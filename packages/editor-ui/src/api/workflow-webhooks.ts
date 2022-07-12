7b1cc1f5f484ba9a3de2e3b5c70ff7c43714f95d
Milorad Filipovic <milorad@n8n.io>
2022-07-12 16:36:07 +0200

    💩 Calling temporary workflow until the real one is implemented

--- packages/editor-ui/src/api/workflow-webhooks.ts
+++ packages/editor-ui/src/api/workflow-webhooks.ts
@@ -1,9 +1,10 @@
 import { IOnboardingCallPromptResponse, IUser } from "@/Interface";
 import { get } from "./helpers";
 
+// https://milorad.app.n8n.cloud/webhook/46a6a97e-dff8-43bb-8085-535a9455bb0a
 // TODO: Replace this with production when the workflow is ready
-const N8N_WF_WEBHOOK_BASE_URL = 'https://internal.users.n8n.cloud/webhook-test';
-const N8N_ONBOARDING_CALL_PROMPT_WEBHOOK_PATH = '56f4e271-4e9f-4b38-b21e-0fa2d5113238';
+const N8N_WF_WEBHOOK_BASE_URL = 'https://milorad.app.n8n.cloud/webhook'; //'https://internal.users.n8n.cloud/webhook-test';
+const N8N_ONBOARDING_CALL_PROMPT_WEBHOOK_PATH = '46a6a97e-dff8-43bb-8085-535a9455bb0a'; //'56f4e271-4e9f-4b38-b21e-0fa2d5113238';
 
 export async function fetchNextOnboardingPrompt(instanceId: string, currentUer: IUser): Promise<IOnboardingCallPromptResponse> {
 	return await get(
